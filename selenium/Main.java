b71ca1b6c6d211248424804f41a1ea92b942141f
Fabio Sbano <fsbano@yahoo.com>
2023-10-25 04:04:38 -0300

    Updated

--- selenium/Main.java
+++ selenium/Main.java
@@ -1,6 +1,11 @@
+import java.io.File;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.Augmenter;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -9,7 +14,15 @@ public class Main {
   public static void main(String[] args) throws MalformedURLException {
     ChromeOptions chromeOptions = new ChromeOptions();
     WebDriver driver = new RemoteWebDriver(new URL("http://eugenia:4444"), chromeOptions);
+    driver = new Augmenter().augment(driver);
     driver.get("http://www.google.com");
+    try {
+    File srcFile = ((TakesScreenshot) driver)
+        .getScreenshotAs(OutputType.FILE);
+    FileUtils.copyFile(srcFile, new File("./01.png"));
+    } catch (Exception e) {
+      System.out.println(e);
+    }
     System.out.println(driver.getTitle());
     driver.quit();
   }
